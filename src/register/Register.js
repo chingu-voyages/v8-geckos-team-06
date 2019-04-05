428afb0a123f8ca0f59f19e5646b2d8ae7a2a30c
pablowbk <pablo.weisbek@gmail.com>
2019-04-05 12:23:11 -0300

    reset input forms to match app style

--- src/register/Register.js
+++ src/register/Register.js
@@ -4,35 +4,40 @@ import './Register.css';
 const Register = ({ onRouteChange }) => {
   return (
     <main className="Register">
-      <form className="measure center" name="register">
-        <fieldset
-          id="sign_up"
-        className="ba b--transparent ph0 mh0">
-          <legend className="f4 fw6 ph0 mh0">Register</legend>
-          <div className="mt3">
-            <label
-              className="db fw6 lh-copy f6"
-            for="name">
-              Name
-            </label>
-            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name" />
+      <form className="form">
+
+        <h3 className="form-title">Register</h3>
+
+        <div id="sign_up" className="sign_up">
+
+          <div className="input-container">
+            <label className="name" htmlFor="name">Name</label>
+            <input className="form-input" type="text" name="name"  id="name" />
           </div>
-          <div className="mt3">
-            <label
-              className="db fw6 lh-copy f6"
-            for="email-address">
-              Email
-            </label>
-            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+
+          <div className="input-container">
+            <label className="email" htmlFor="email">Email</label>
+            <input className="form-input" type="email" name="email"  id="email" />
           </div>
-          <div className="mv3">
-            <label className="db fw6 lh-copy f6" for="password">Password</label>
-            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+
+          <div className="input-container">
+            <label className="password" htmlFor="password">Password</label>
+            <input className="form-input" type="password" name="password"  id="password" />
           </div>
-        </fieldset>
-        <div className="">
-          <input onClick={() => onRouteChange('signin')} className="b ph3 pv2 input-reset ba b--black bg-transparent pointer f6 dib" type="submit" value="Sign Up" />
-          <p onClick={() => onRouteChange('home')} className="f6 link dim black db" style={{cursor: 'pointer'}}>Home</p>
+
+          <div className="input-container">
+            <label className="confirm-password" htmlFor="confirm-password">Confirm Password</label>
+            <input className="form-input" type="password" name="confirm-password"  id="confirm-password" />
+          </div>
+
+        </div>
+
+        <div className="form-buttons">
+          <input onClick={() => onRouteChange('home')} className="register-btn" type="submit" value="Sign Up" />
+        </div>
+
+        <div className="form-links">
+          <p onClick={() => onRouteChange('home')} className="form-link">Home </p>
 
         </div>
       </form>
--- src/signin/Signin.js
+++ src/signin/Signin.js
@@ -6,25 +6,26 @@ const Signin = ({ onRouteChange }) => {
     <main className="Signin">
       <form className="form">
 
-        <fieldset id="sign_up" className="sign_up">
+        <fieldset id="sign_in" className="sign_in">
           <legend className="form-title">Sign In</legend>
-          <div className="inputs-container">
+
+          <div className="input-container">
             <label className="email" htmlFor="email">Email</label>
             <input className="form-input" type="email" name="email"  id="email" />
           </div>
-          <div className="mv3">
-            <label className="form-input" htmlFor="password">
-              Password
-            </label>
+
+          <div className="input-container">
+            <label className="password" htmlFor="password">Password</label>
             <input className="form-input" type="password" name="password"  id="password" />
           </div>
+
         </fieldset>
 
-        <div className="inputs-container">
-          <input onClick={() => onRouteChange('home')} className="form-btn" type="submit" value="Sign in" />
+        <div className="form-buttons">
+          <input onClick={() => onRouteChange('home')} className="login-btn" type="submit" value="Log In" />
         </div>
 
-        <div className="lh-copy mt3 pointer">
+        <div className="form-links">
           <p onClick={() => onRouteChange('register')} className="form-link">Register </p>
           <p onClick={() => onRouteChange('home')} className="form-link">Home </p>
 
