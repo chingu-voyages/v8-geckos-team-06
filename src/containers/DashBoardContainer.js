3cbbc6c419466a2e9683d0e960613aa45e60b95c
sammartfrank <franksammartino7@gmail.com>
2019-04-29 09:22:00 -0300

    'lunes1

--- src/containers/DashBoardContainer.js
+++ src/containers/DashBoardContainer.js
@@ -13,23 +13,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-	getUserData: (id) => {
-		const api = MhubApi();
-		dispatch({
-			type: 'LOADER_ON',
-		})
-		Promise.all([
-			api.checkUser(id)
-			]).then( (user) => {
-				dispatch({
-					type: 'GET_USER',
-					user
-				})
-				dispatch({
-					type: 'LOADER_OFF'
-				})
-			})
-	}
+
 	})
 
 
