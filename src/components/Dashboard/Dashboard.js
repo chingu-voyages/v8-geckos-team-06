b336cc5cc00e3b804581571cbb93863f9c29eb08
pablowbk <pablo.weisbek@gmail.com>
2019-04-30 11:02:45 -0300

    minor layout update to register form

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,36 +2,36 @@ import React from 'react';
 import Meds from '../Meds/Meds.js';
 
 
-const Dashboard = ({userId, token, isLogged, onLogOut, user}) => { 
+const Dashboard = ({userId, token, isLogged, onLogOut, user}) => {
   return (
   	<div className="container-fluid">
-					<h1>THE DASHBOARD</h1>
-					<h3>Welcome User: {userId}</h3>
-					<button
-								className="btn btn-outline-danger btn-lg"
-								onClick={ e => {
-									e.preventDefault()
-									onLogOut()
-								}}
-							>Log Out
-							</button>
-					<hr />
-				<div className="Searchbar">
-					<input type="search" placeholder="Search Med"/>
-				</div>
-				<div className="sidePanel">
-					SidePanel
-				</div>
-				<div className="listofMeds">
-					<Meds token={token} userId={userId} />
-				</div>
-				<div>
-				</div>
-				<div>
+      <h1>THE DASHBOARD</h1>
+      <h3>Welcome User: {userId}</h3>
+      <button
+        className="btn btn-outline-danger btn-lg"
+        onClick={ e => {
+          e.preventDefault()
+          onLogOut()
+        }}
+      >Log Out
+      </button>
+      <hr />
+      <div className="Searchbar">
+        <input type="search" placeholder="Search Med"/>
+      </div>
+      <div className="sidePanel">
+        SidePanel
+      </div>
+      <div className="listofMeds">
+        <Meds token={token} userId={userId} />
+      </div>
+      <div>
+      </div>
+      <div>
 					<p>Lorem ipsum dolor sit amet,
-					 consectetur adipisicing elit. 
-					 Eius quam repudiandae temporibus 
-					 voluptatem porro! Atque at, laboriosam 
+					 consectetur adipisicing elit.
+					 Eius quam repudiandae temporibus
+					 voluptatem porro! Atque at, laboriosam
 					 dolores nulla itaque, consectetur a
 					 recusandae vero officia eius error
 					 distinctio doloremque, tempora!</p>
