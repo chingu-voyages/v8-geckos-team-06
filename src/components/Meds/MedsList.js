00ce04969aee90f58ad3b1f4fb64eee07825b836
pablowbk <pablo.weisbek@gmail.com>
2019-05-02 08:10:25 -0300

    add key to medlist items

--- src/components/Meds/MedsList.js
+++ src/components/Meds/MedsList.js
@@ -5,13 +5,17 @@ const MedsList = ({meds}) => {
     <div className="meds-list">
     	{ meds && meds.map((med) => {
         return med.map((it)=> {
-          return <div key={it.id} className="med-card">
-    					<h2 style={{color:'darkred'}}>{it.manufacturer}</h2>
-    					<p style={{color:'darkred'}}>{it.manufacturer}</p>
-    				</div>
-    			})
-    	})}
+          console.log(it);
+          return (
+            <div key={it._id} className="med-card">
+              <h2 style={{color:'darkred'}}>{it.manufacturer}</h2>
+              <p style={{color:'darkred'}}>{it.manufacturer}</p>
+            </div>
+          );
+        })
+    	})
+      }
     </div>
-  )
+  );
 }
 export default MedsList;
